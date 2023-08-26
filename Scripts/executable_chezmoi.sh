328b371fdb1096dfc353712e89a01187e3dc557c
dok4440 <dokimakimaki@gmail.com>
2023-08-26 18:11:11 -0400

    Update pkglist

--- Scripts/executable_chezmoi.sh
+++ Scripts/executable_chezmoi.sh
@@ -3,6 +3,13 @@ if [[ $EUID -eq 0 ]]; then
 echo "This script cannot be run as root."
 exit 1
 fi
+
+# https://wiki.archlinux.org/title/Pacman/Tips_and_tricks#List_of_installed_packages
+pacman -Qqe > ~/.pkg_list/pkglist
+pacman -Qqm > ~/.pkg_list/pkglist_foreign
+
+
+# commit and push changes to dotfiles
 cd ~/.local/share/chezmoi
 chezmoi re-add
 read -p "Commit message: " MESSAGE
