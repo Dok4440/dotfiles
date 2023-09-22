8fe6cd8cd7b3bc6c8dd022977b5ea3bd0b25d7b9
dok4440 <dokimakimaki@gmail.com>
2023-09-22 13:24:31 +0200

    Add ln to scripts

--- Scripts/executable_create-symlinks.sh
+++ Scripts/executable_create-symlinks.sh
@@ -4,9 +4,12 @@
 if [ "$EUID" -eq 0 ]; then
     user_home=$(eval echo ~$(logname))
 else
-    echo "Run this script as root."
+    echo "Run this script with sudo."
     exit
 fi
+
+# make necessary directories
+mkdir -p /usr/share/backgrounds/i3lock
 
 # Create symbolic links with the replaced path
 ln -s "$user_home/Scripts/cp-p.sh" /usr/local/bin/cp-p
@@ -14,3 +17,4 @@ ln -s "$user_home/Scripts/mv-p.sh" /usr/local/bin/mv-p
 ln -s "$user_home/Scripts/GPUdisable.sh" /usr/local/bin/GPUdisable
 ln -s "$user_home/Scripts/GPUenable.sh" /usr/local/bin/GPUenable
 ln -s "$user_home/Scripts/chezmoi.sh" /usr/local/bin/dotfiles
+ln -s "$user_home/Pictures/i3lock/cat-peek-adjusted.png" /usr/share/backgrounds/i3lock/cat-peek-adjusted.png
